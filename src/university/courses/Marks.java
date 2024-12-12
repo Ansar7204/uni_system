e813ee7b769f46a2a0c3720e41c0182724de099a
gulyakrutaya <gulnazbekturgan2@gmail.com>
2024-12-12 15:54:46 +0500

    jkndewjnd

--- src/university/courses/Marks.java
+++ src/university/courses/Marks.java
@@ -1,11 +1,7 @@
 package university.courses;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
 public enum Marks
 {
 	A, A-, B, B-, C+, C, C-, D, F;
 }
+//fjnehuifn
