2798b93c1d959fadbc2a0c2a98194979750bd0e0
yergaliyevan <yergaliyevan@icloud.com>
2024-12-13 04:31:02 +0500

    prob

--- src/university/courses/Marks.java
+++ src/university/courses/Marks.java
@@ -1,5 +1,14 @@
 package university.courses;
 
 public enum Marks {
-	A, B, C, D, F;
+	A(4.0), B(3.0), C(2.0), D(1.0), F(0.0);
+	private final double gpaValue;
+
+	Marks(double gpaValue) {
+		this.gpaValue = gpaValue;
+	}
+
+	public double getGpaValue() {
+		return gpaValue;
+	}
 }
--- src/university/courses/Transcript.java
+++ src/university/courses/Transcript.java
@@ -1,16 +1,30 @@
 package university.courses;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Transcript {
-	public Marks marks;
-	public Transcript(){
-		super();
+	private Map<Course, Marks> courseMarks;
+
+	public Transcript() {
+		this.courseMarks = new HashMap<>();
 	}
 
+	public void addCourseMarks(Course course, Marks marks) {
+		courseMarks.put(course, marks);
+	}
 
-	
 	public double calculateGPA() {
-		return 0;
-	}
-}
+		if (courseMarks.isEmpty()) {
+			return 0.0;
+		}
 
+		double totalGpa = 0.0;
+		for (Marks marks : courseMarks.values()) {
+			totalGpa += marks.getGpaValue();
+		}
+
+		return totalGpa / courseMarks.size();
+	}
+
+}
